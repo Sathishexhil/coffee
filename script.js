7fcae6cb011d5502c17b25a1d3fb1e3306b96139
Sathishexhil <40395456+Sathishexhil@users.noreply.github.com>
2023-08-08 01:55:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu =  document.querySelector('.menu');
+let navbar = document.querySelector('.navbar')
+
+
+
+menu.addEventListener('click',function(){
+    navbar.classList.toggle('active');
+})
